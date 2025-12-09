1aea2f77e5acdc3a418d7c21c473b2876f02df9e
Aurelio Malheiros <aureliomalheiros@gmail.com>
2025-12-08 22:31:55 -0300

    go routine detailed

---
+++ languages/golang/goroutines/basic-goroutine.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"runtime"
+	"unsafe"
+)
+
+
+func myFunction() {
+	fmt.Printf("[Goroutine ID: %d]\n", getGID())
+	fmt.Printf("[Goroutine] Number of CPUs: %d\n", runtime.NumCPU())
+	fmt.Printf("[Goroutine] Pointer size: %d bytes\n", unsafe.Sizeof(uintptr(0)))
+	fmt.Printf("[Goroutine] Go version: %s\n", runtime.Version())
+	fmt.Printf("[Goroutine] OS/Arch: %s/%s\n", runtime.GOOS, runtime.GOARCH)
+	fmt.Printf("[Goroutine] Number of Goroutines: %d\n", runtime.NumGoroutine())
+	fmt.Println("Goroutine completed.")
+}
+
+func getGID() uint64 {
+
+	buf := make([]byte, 64)
+	buf  = buf[:runtime.Stack(buf, false)]
+	var id uint64
+	fmt.Sscanf(string(buf), "goroutine %d ", &id)
+	return id
+
+}
+
+func main() {
+	fmt.Printf("[Main] Goroutine ID: %d\n", getGID())
+    fmt.Printf("Número de goroutines: %d\n", runtime.NumGoroutine())
+    fmt.Printf("Número de threads: %d\n", runtime.GOMAXPROCS(0))
+
+	//Analise memory
+	var memBefore runtime.MemStats
+	runtime.ReadMemStats(&memBefore)
+	fmt.Printf("Memory Alloc before: %d bytes\n", memBefore.Alloc)
+
+	//Create a goroutine
+	fmt.Println("Starting goroutine...")
+	go myFunction()
+
+	//Wait for goroutine to finish
+	time.Sleep(20 * time.Second)
+
+	fmt.Println("After goroutine execution:")
+	fmt.Printf("Number of goroutines: %d\n", runtime.NumGoroutine())
+
+	//Analyze memory after goroutine execution
+	var memAfter runtime.MemStats
+	runtime.ReadMemStats(&memAfter)
+	fmt.Printf("Memory Alloc after: %d bytes\n", memAfter.Alloc)
+	fmt.Printf("Memory Alloc difference: %d bytes\n", memAfter.Alloc - memBefore.Alloc)
+
+	//Wait before exiting
+	time.Sleep(5 * time.Second)
+	
+	fmt.Printf("Number of goroutines before exit: %d\n", runtime.NumGoroutine())
+	fmt.Println("Main function completed.")
+
+}
